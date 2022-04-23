8618d9e7e5aec029b5a293d95199211128fa35db
Samuel-Tobi <oluwatobi9990@gmail.com>
2022-04-23 18:43:33 +0100

    New members and Latest transaction boxes added

--- src/component/widgetLg/WidgetLg.jsx
+++ src/component/widgetLg/WidgetLg.jsx
@@ -1,5 +1,69 @@
 import './widgetLg.css';
+import image1 from './Images/MF-Doom.png';
+import image2 from './Images/nas.png';
+import image3 from './Images/royce-da-59.jpeg';
+import image4 from './Images/sage-francis.jpeg';
+// import image5 from './Images/tech-n9ne.jpg';
 
 export default function WidgetLg() {
-  // return <div className='widgetLg'>Large</div>;
+  const Button = ({ type }) => {
+    return <button className={'widgetLgButton ' + type}>{type}</button>;
+  };
+  return (
+    <div className='widgetLg'>
+      <h3 className='widgetLgTitle'>Latest transactions</h3>
+      <table className='widgetLgTable'>
+        <tr className='widgetLgTr'>
+          <th className='widgetLgTh'>Customer</th>
+          <th className='widgetLgTh'>Date</th>
+          <th className='widgetLgTh'>Amount</th>
+          <th className='widgetLgTh'>Status</th>
+        </tr>
+        <tr className='widgetLgTr'>
+          <td className='widgetLgUser'>
+            <img src={image1} alt='' className='widgetLgImg' />
+            <span className='widgetLgName'>MF - Doom</span>
+          </td>
+          <td className='widgetLgDate'>2 June 2021</td>
+          <td className='widgetLgAmount'>$122.00</td>
+          <td className='widgetLgStatus'>
+            <Button type='Approved' />
+          </td>
+        </tr>
+        <tr className='widgetLgTr'>
+          <td className='widgetLgUser'>
+            <img src={image2} alt='' className='widgetLgImg' />
+            <span className='widgetLgName'>Nas</span>
+          </td>
+          <td className='widgetLgDate'>2 June 2021</td>
+          <td className='widgetLgAmount'>$122.00</td>
+          <td className='widgetLgStatus'>
+            <Button type='Declined' />
+          </td>
+        </tr>
+        <tr className='widgetLgTr'>
+          <td className='widgetLgUser'>
+            <img src={image3} alt='' className='widgetLgImg' />
+            <span className='widgetLgName'>Royce - Da - 56</span>
+          </td>
+          <td className='widgetLgDate'>2 June 2021</td>
+          <td className='widgetLgAmount'>$122.00</td>
+          <td className='widgetLgStatus'>
+            <Button type='Pending' />
+          </td>
+        </tr>
+        <tr className='widgetLgTr'>
+          <td className='widgetLgUser'>
+            <img src={image4} alt='' className='widgetLgImg' />
+            <span className='widgetLgName'>Savage - Francis</span>
+          </td>
+          <td className='widgetLgDate'>2 June 2021</td>
+          <td className='widgetLgAmount'>$122.00</td>
+          <td className='widgetLgStatus'>
+            <Button type='Approved' />
+          </td>
+        </tr>
+      </table>
+    </div>
+  );
 }
--- src/component/widgetSm/WidgetSm.jsx
+++ src/component/widgetSm/WidgetSm.jsx
@@ -1,5 +1,71 @@
 import './widgetSm.css';
-
+import { Visibility } from '@mui/icons-material';
+import image1 from './Images/chali-2na.jpg';
+import image2 from './Images/eminem.jpg';
+import image3 from './Images/jay-z.jpg';
+import image4 from './Images/kool-g-rap.png';
+import image5 from './Images/lil-wayne.jpg';
 export default function WidgetSm() {
-  // return <div className='widgetSm'>small</div>;
+  return (
+    <div className='widgetSm'>
+      <span className='widgetSmTitle'>New Join Members</span>
+      <ul className='widgetSmList'>
+        <li className='widgetSmListItem'>
+          <img src={image1} alt='' className='widgetSmImg' />
+          <div className='widgetSmUser'>
+            <span className='widgetSmUsername'>Chali 2na</span>
+            <span className='widgetUserTitle'>Frontend Developer</span>
+          </div>
+          <button className='widgetSmButton'>
+            <Visibility className='widgetSmIcon' />
+            Display
+          </button>
+        </li>
+        <li className='widgetSmListItem'>
+          <img src={image2} alt='' className='widgetSmImg' />
+          <div className='widgetSmUser'>
+            <span className='widgetSmUsername'>Eminem</span>
+            <span className='widgetUserTitle'>Fullstack Developer</span>
+          </div>
+          <button className='widgetSmButton'>
+            <Visibility className='widgetSmIcon' />
+            Display
+          </button>
+        </li>
+        <li className='widgetSmListItem'>
+          <img src={image3} alt='' className='widgetSmImg' />
+          <div className='widgetSmUser'>
+            <span className='widgetSmUsername'>Jay Z</span>
+            <span className='widgetUserTitle'>Backend Developer</span>
+          </div>
+          <button className='widgetSmButton'>
+            <Visibility className='widgetSmIcon' />
+            Display
+          </button>
+        </li>
+        <li className='widgetSmListItem'>
+          <img src={image4} alt='' className='widgetSmImg' />
+          <div className='widgetSmUser'>
+            <span className='widgetSmUsername'>Kool-G</span>
+            <span className='widgetUserTitle'>Product Designer</span>
+          </div>
+          <button className='widgetSmButton'>
+            <Visibility className='widgetSmIcon' />
+            Display
+          </button>
+        </li>
+        <li className='widgetSmListItem'>
+          <img src={image5} alt='' className='widgetSmImg' />
+          <div className='widgetSmUser'>
+            <span className='widgetSmUsername'>Lil Wayne</span>
+            <span className='widgetUserTitle'>Digital Marketer</span>
+          </div>
+          <button className='widgetSmButton'>
+            <Visibility className='widgetSmIcon' />
+            Display
+          </button>
+        </li>
+      </ul>
+    </div>
+  );
 }
