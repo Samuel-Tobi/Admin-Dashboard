179b6ee1d4aea4a7d85503b9dbe9143d0f7c4144
Samuel-Tobi <oluwatobi9990@gmail.com>
2022-04-23 01:16:41 +0100

    Completed User Analytics Box

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,24 @@
+import Chart from '../../component/chart/Chart';
 import FeaturedInfo from '../../component/featuredInfo/FeaturedInfo';
 import './home.css';
+import { UserData } from '../../dummyData';
+import WidgetSm from '../../component/widgetSm/WidgetSm';
+import WidgetLg from '../../component/widgetLg/WidgetLg';
 
 export default function Home() {
   return (
     <div className='home'>
       <FeaturedInfo />
+      <Chart
+        data={UserData}
+        title='User Analytics'
+        grid
+        dataKey='Active User'
+      />
+      <div className='homeWidgets'>
+        <WidgetSm />
+        <WidgetLg />
+      </div>
     </div>
   );
 }
